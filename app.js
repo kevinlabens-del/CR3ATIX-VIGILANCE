0056f78a09d567d8c835a6e7aa4c818d085eb851
kevinlabens-del <kevin.labens@gmail.com>
2026-09-12 21:31:02 +0200

    fix: load non-blocking onboarding v1.9.6

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ document.head.appendChild(runtimeStyles);
 
 const onboardingStyles = document.createElement("link");
 onboardingStyles.rel = "stylesheet";
-onboardingStyles.href = "./onboarding-v19.css?v=195";
+onboardingStyles.href = "./onboarding-v19.css?v=196";
 document.head.appendChild(onboardingStyles);
 
 import "./recommendations-v17.js";
 import "./runtime-v18.js";
-import "./onboarding-v195.js";
+import "./onboarding-v196.js";
